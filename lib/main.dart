fd2fbb1402b3a225f81f0d8eaec562baa955ab57
Jhonatan Edward <jon.edward@outlook.com>
2020-07-20 01:25:31 -0300

    Alinhando eixo principal e eixo cruzado.

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,26 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Despesas Pessoais'),
+        title: Text(
+          'Despesas Pessoais',
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.blue,
+              child: Text('Gráfico'),
+              elevation: 5.0,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+            elevation: 6.0,
+          )
+        ],
       ),
     );
   }
